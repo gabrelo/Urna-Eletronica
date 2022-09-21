b2e1af7936e05eb2314981b886936a3f9d5c87a5
Gabriel do Carmo Sousa <92333990+gabrelo@users.noreply.github.com>
2022-09-21 15:07:36 -0300

    first commit

---
+++ etapas.js
@@ -0,0 +1,50 @@
+let etapas = [
+    {
+        titulo: 'VEREADOR',
+        numeros: 5,
+        candidatos: [
+            {
+                numero: '38111',
+                nome: 'Fulano de tal',
+                partido: 'ABC',
+                fotos:[
+                    {url:'candidato1.png', legenda:'Vereador', small: false}
+                ]
+            },
+            {
+                numero: '77222',
+                nome: 'Beltrano de tal',
+                partido: 'DEFG',
+                fotos:[
+                    {url:'candidato2.png', legenda:'Vereador', small: false}
+                ]
+            },
+        ]
+    },
+    {
+        titulo: 'PREFEITO',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: '99',
+                nome: 'Ciclano',
+                partido: 'ABC',
+                vice: 'Cic',
+                fotos:[
+                    {url:'candidato3.png', legenda: 'Prefeito'},
+                    {url:'candidato4.png', legenda: 'Vice-Prefeito', small: true}
+                ]
+            },
+            {
+                numero: '84',
+                nome: 'Zulano',
+                partido: 'QWERTY',
+                vice: 'Zul',
+                fotos:[
+                    {url:'candidato5.png', legenda: 'Prefeito'},
+                    {url:'candidato6.png', legenda: 'Vice-Prefeito', small: true}
+                ]
+            },
+        ]
+    }
+];
